e43ee945d9004621429f6f0b9e979e824a0d3b98
mariangelamuscolino <muscolinomariaangela@gmail.com>
2026-03-18 14:54:09 +0100

    Mary: Aggiunta inputbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,21 @@
 import Navbar from "./components/Navbar";
-import NewsCard from "./components/NewsCard"; // Prendi la figurina dalla scatola!
+import InputBar from "./components/InputBar"; // Prendi la scatola!
+import NewsCard from "./components/NewsCard";
 
 function App() {
   return (
     <div className="min-h-screen bg-gray-50">
       <Navbar />
       
-      <main className="container mx-auto p-10">
-        <h2 className="text-3xl font-black mb-8 text-center">Ultime Notizie ✨</h2>
+      <main className="container mx-auto">
+        {/* La scatola dei messaggi va qui in alto! */}
+        <InputBar />
         
-        {/* Ecco la tua NewsCard! */}
-        <div className="flex justify-center">
+        <h2 className="text-3xl font-black mb-8 text-center text-gray-800">
+          Le tue figurine News ✨
+        </h2>
+        
+        <div className="flex justify-center pb-20">
           <NewsCard />
         </div>
       </main>
